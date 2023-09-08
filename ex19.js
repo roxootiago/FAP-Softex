7d35579c9bf27f56831955a6517d0d08c94c2326
roxootiago <tiago.roxo@hotmail.com>
2023-09-08 20:41:56 -0300

    upload

---
+++ ex19.js
@@ -0,0 +1,17 @@
+function bomdia() {
+  return `Bom dia`;
+}
+
+console.log(bomdia());
+
+function boaTarde(nome) {
+  return `Boa tarde, ${nome}`;
+}
+
+console.log(boaTarde("Tiago"));
+
+const boaNoite = (nome) => {
+  return `Boa noite, ${nome}`;
+};
+
+console.log(boaNoite("Tiago"));
