44154fcc651e8a4de3f0dcfec73124ebac496e28
roxootiago <tiago.roxo@hotmail.com>
2023-09-15 12:15:56 -0300

    uploda

---
+++ exercicios/ex29.js
@@ -0,0 +1,15 @@
+let cad = [
+    {nome: 'José', idade: 19, salario: 1900, ensinoMedio: true},
+    {nome: 'David', idade: 22, salario: 2500, ensinoMedio: false},
+    {nome: 'Maria', idade: 25, salario: 3400, ensinoMedio: true},
+    {nome: 'Igor', idade: 18, salario: 2100, ensinoMedio: false},
+]
+
+/* utilizando matrizes */
+
+let cad2 = [
+    ['José',19,1900,true],
+    ['David',22,2500,false],
+    ['Maria',25,3400,true],
+    ['Igor',18,2100,false],
+]
